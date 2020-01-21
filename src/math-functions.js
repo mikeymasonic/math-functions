794fd54370c8fa96eecd1e74e853fa51133e63b3
Michael Romay <amerror@gmail.com>
2020-01-21 10:14:43 -0800

    fixed problem4

--- src/math-functions.js
+++ src/math-functions.js
@@ -13,7 +13,6 @@ example and uses the values that were input into the function:
 
 export function sum(a, b) {
     return [a + b, `The sum of ${a} and ${b} is ${a + b}.`];
-
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
@@ -28,7 +27,6 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 export function multiply(a, b) {
     return [a * b, `The product of ${a} and ${b} is ${a * b}.`];
-
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
@@ -77,7 +75,8 @@ to use the + operator for string concatenation.
 */
 
 export function sumArrayWithThreeNumbers(sumArr) {
-
+    const sumTotal = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
+    return [sumTotal, `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumTotal} is their sum.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
